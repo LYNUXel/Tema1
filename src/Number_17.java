8bf9e586fbe23d4e693219b971fde29181dbd27f
LYNUXel <57704509+LYNUXel@users.noreply.github.com>
2019-12-24 01:37:59 +0200

    Update Number_17.java

--- src/Number_17.java
+++ src/Number_17.java
@@ -3,30 +3,10 @@
 import java.util.Scanner;
 
 public class Number_17 {
-    public static int par(int a, int b) {
-        return a%2;
-    }
-
     public static void main(String[] args) {
         System.out.println("Exercitiu 17. Cititi numere de la consola pana cand se introduce un numar par");
         System.out.println("-------------");
         System.out.println("Introdu numere: ");
         Scanner in = new Scanner(System.in);
-
-        for (int i = 0; i < 100; i++) {
-            int a = in.nextInt();
-            int b = in.nextInt();
-            if (a % 2 == b) {
-                System.out.println("" + a);
-            } else {
-                for (b = 0; b > a; i++) {
-                    System.out.println(" " + b);
-                }
-//        return;
-//            }
-//            int result = par(a+b/2, b/2);
-//        }
-            }
-        }
     }
 }
